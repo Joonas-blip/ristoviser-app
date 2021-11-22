a9507c76aded7d26ee558581a1ad2e43c55d2316
Naima Duyser <naima.fatimaduyser@gmail.com>
2021-11-22 17:56:41 +0100

    Gemfile, schema

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 2021_11_22_155646) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
+    t.string "avatar_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
