fc0a9e2c200a36e0edd11377bcd47a98ac72cf08
Joonas Villanen <joonas.villa@gmail.com>
2021-11-23 15:04:48 +0100

    fix

--- db/migrate/20211122151737_devise_create_users.rb
+++ db/migrate/20211122151737_devise_create_users.rb
@@ -14,12 +14,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Added
-      t.string :first_name
-      t.string :last_name
-      t.string :location
-      t.string :avatar_url
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
