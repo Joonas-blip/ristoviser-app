3536d5469491aea4573a5d1a69aa48db4faa59d5
Joonas Villanen <joonas.villa@gmail.com>
2021-11-23 09:01:25 +0100

    typo fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :collection_restaurants
   has_many :notes
-  validates :name, :address, presence: true, uniqieness: true
+  validates :name, :address, presence: true, uniqueness: true
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 end
