ea34d13f6f70a9d6d304402fc0af277daf318ecb
Joonas Villanen <joonas.villa@gmail.com>
2021-11-23 14:27:30 +0100

    retausrants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,6 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    @user = current_user
     @note = Note.find_by(user: current_user, restaurant: @restaurant)
   end
 
