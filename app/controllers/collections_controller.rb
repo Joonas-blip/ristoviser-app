92e146f2f59b34d32850fed20c8f8ea6a14398d9
Joonas Villanen <joonas.villa@gmail.com>
2021-11-26 17:47:15 +0100

    fixes

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,5 @@
 class CollectionsController < ApplicationController
-  before_action :set_collection, only: [:show, :map]
+  before_action :set_collection, only: [:show]
   skip_before_action :authenticate_user!, only: :map
 
   def index
@@ -26,6 +26,7 @@ class CollectionsController < ApplicationController
   end
 
   def map
+    @collection = Collection.find(params[:collection_id])
     @restaurants = @collection.restaurants
 
     @markers = @restaurants.geocoded.map do |restaurant|
