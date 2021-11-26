e31ab45e73e6a2b8b4aa92a222d937e74e10b353
Joonas Villanen <joonas.villa@gmail.com>
2021-11-26 09:29:24 +0100

    added validation of unique friend pairs

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,8 @@ class Friendship < ApplicationRecord
   belongs_to :user
   validates :friend_id, presence: true
   validates :status, inclusion: { in: STATUS }
+  validates :user, uniqueness: { scope: [:friend_id] }
+
 
   def self.of_status(user, status)
     Friendship.all.select { |friendship| friendship.user == user && friendship.status == status }
