473f684033c8a460a2e9d2c69327f55541f1a9f1
Andrea Corbetta <corbettaandrea.dev@gmail.com>
2021-12-01 17:22:57 +0100

    cloudinary set and code fixes

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,5 +2,6 @@ class Collection < ApplicationRecord
   belongs_to :user
   has_many :collection_restaurants
   has_many :restaurants, through: :collection_restaurants
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
 end
